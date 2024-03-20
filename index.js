bee4be5eca7c0a54bea4bea79efdeff05afcb789
Caesar <ignatiuscaesar16@gmail.com>
2024-03-21 01:41:24 +0300

    Edits the js file

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+// Requirement 1
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; // This should equal 62
+
+// Requirement 2
+const random = Math.floor(Math.random() * 1000) + 1; // Generates a random integer greater than 0
+
+// Requirement 3
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4; // This should equal 4
+
+// Requirement 4
+const max = Math.max(1, 5, 10, 15, 20); // This should return 20 as the highest number
